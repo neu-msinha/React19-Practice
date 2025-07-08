e030638ad98205c3d3a1039c1d21a811d0a7371a
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-09 00:19:11 +0530

    Reverting back to simple counter

--- basic/src/components/playground/state.jsx
+++ basic/src/components/playground/state.jsx
@@ -3,34 +3,16 @@ import {useState} from 'react';
 //Complex State
 
 const StatePlayground = () => {
-    let initialCount = 0;
-  let [state, setState] = useState({
-    count: initialCount,
-    user: "Mayukh",
-  });
+
+  let [count, setCount] = useState(0);
+//   console.log(count);
 
   return (
     < >
-      <h1>{state.user}</h1>
-      <h3>Count: {state.count}</h3>
-      <button onClick={() => setState(prevState => {
-        return {
-          ...prevState,
-          count: prevState.count + 1
-        };
-      })}>Increment</button>
-      <button onClick={() => setState(prevState => {
-        return {
-          ...prevState,
-          count: prevState.count > 0 ? prevState.count - 1 : 0
-        };
-        })}>Decrement</button>
-      <button onClick={() => setState(prevState => {
-        return {
-          ...prevState,
-          count: 0
-        };
-        })}>Reset</button>
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(prevCount => prevCount > 0 ? prevCount - 1 : 0)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </>
   );
 }
