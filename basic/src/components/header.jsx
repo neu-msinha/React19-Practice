7f33dfe196102e888b50b6c5beeab7f85afda0b6
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-08 10:59:51 +0530

    Adding styles to public can be referenced in the index.html and then across the whole app

--- basic/src/components/header.jsx
+++ basic/src/components/header.jsx
@@ -1,21 +1,7 @@
-
-let styles = {
-    header: {
-        background: '#03a9f4',
-
-    },
-    logo: {
-        color: '#fff',
-        fontFamily: 'Anton',
-        textAlign: 'center'
-    }
-}
-
 const Header = () => {
    return (
-    <header style={styles.header}>
-        <h1 style={styles.logo}>My React App</h1>
-        <p style={{ color: '#fff', textAlign: 'center' }}>Welcome to my React application!</p>
+    <header>
+        <div className="logo">LOGO</div>
     </header>
    )
 }
