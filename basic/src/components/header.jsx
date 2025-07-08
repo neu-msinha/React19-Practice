fab8dc6cd9f6801abc8329f8d6d224953cd4aca7
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-08 10:47:38 +0530

    Calling functions in JSX

--- basic/src/components/header.jsx
+++ basic/src/components/header.jsx
@@ -1,6 +1,14 @@
+
+const getTheYear = () => {
+    const newDate = new Date(); 
+    return newDate.getFullYear()
+}
+
+// Can use inline return statement or call a function in line 10.
+
 const Header = () => (
     <div>
-        Header
+        The date is {getTheYear()}.
     </div>
 )
 
