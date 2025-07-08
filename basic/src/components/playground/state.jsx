3128bbe7fbef4c18435bef46db7eb32f12c5cc93
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-09 00:00:12 +0530

    Added check for non negative counter

--- basic/src/components/playground/state.jsx
+++ basic/src/components/playground/state.jsx
@@ -9,7 +9,7 @@ const StatePlayground = () => {
     < >
       <h3>Count: {count}</h3>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(prevCount => prevCount > 0 ? prevCount - 1 : 0)}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
     </>
   );
