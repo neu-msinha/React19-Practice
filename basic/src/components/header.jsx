7ea2e0d1b2ff37bbba70103b7aaae997ba5b8f6b
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-08 14:09:10 +0530

    Synthetic Events in React

--- basic/src/components/header.jsx
+++ basic/src/components/header.jsx
@@ -1,7 +1,24 @@
 const Header = () => {
+
+    // synthetic events in react (onChange, onClick, etc.)
+
+   const handleChange = (event) => {
+    console.log(event.target.value);
+   }
+
    return (
     <header>
-        <div className="logo">LOGO</div>
+        <div className="logo"
+            onClick={()=>console.log('I was clicked!')}
+            onPointerEnter={()=>console.log('Pointer entered!')}
+            >
+        LOGO
+        </div>
+
+        <input onChange={handleChange}
+               onFocus={e => console.log('onFocus')}
+               onBlur={e => console.log('onBlur')}
+        ></input>
     </header>
    )
 }
