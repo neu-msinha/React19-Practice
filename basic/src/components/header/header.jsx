ab9ef99ed8808ed9b4891d4c7cf2b38b5137c0e1
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-09 00:23:42 +0530

    UseState in header

--- basic/src/components/header/header.jsx
+++ basic/src/components/header/header.jsx
@@ -1,11 +1,14 @@
 import Navigation from './nav.jsx';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [keywords, setKeywords] = useState('');
 
     // synthetic events in react (onChange, onClick, etc.)
 
    const handleChange = (event) => {
-    console.log(event.target.value);
+         setKeywords(event.target.value);
    }
 
    return (
@@ -14,6 +17,7 @@ const Header = () => {
         Awesome News
         </div>
         <input onChange={handleChange}/>
+        The keywords are: {keywords}
         <Navigation />
     </header>
    )
