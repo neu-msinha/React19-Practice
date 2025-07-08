0f0e3c07e48d7758de7971566b2f45228309ed78
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-08 10:34:09 +0530

    Initialised basic repo

---
+++ basic/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
