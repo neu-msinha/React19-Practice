5bb847be9db6085481861e6872e17be4168e7495
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-18 01:03:07 +0530

    CSSTransition, add and renmove class

--- transitions/src/components/CSStransition.jsx
+++ transitions/src/components/CSStransition.jsx
@@ -1,7 +1,34 @@
+import { useState, useRef } from "react";
+import { CSSTransition } from "react-transition-group";
+
+
+// classes that it will add EXITING example: (same for entering)
+
+// cssSq-exit cssSq-exit-active
+// cssSq-exit-done
+
 const CssTr = () => {
+
+  let [show, setShow] = useState(false);
+  const nodeRef = useRef(null);
+
+  const toggleShow = () => {
+    setShow(!show);
+  };
+
   return (
     <>
-      <div>CSS TRANSITIONS</div>
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={show}
+      timeout={500}
+      classNames="cssSq"
+    >
+      <div ref={nodeRef} className="cssSq">
+        Hello
+      </div>
+    </CSSTransition>
+      <button className="btn btn-primary" onClick={toggleShow}>Show</button>
     </>
     )
 };
