de496106275aa83b0ad6302c79da71a6b4d9703a
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-18 00:32:03 +0530

    Added basic block with show toggle

--- transitions/src/components/Transition.jsx
+++ transitions/src/components/Transition.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+// import {Transition} from "react-transition-group";
+
 const TransitionComp = () => {
+  let [show, setShow] = useState(true);
+
+  const toggleShow = () => {
+    setShow(!show);
+  };
+
   return (
     <>
+      {show && 
       <div style={{
         background: 'red',
         height: '100px',
       }}>
+        </div>
+      }
 
-      </div>
+      <button className="btn btn-primary" onClick={toggleShow}>Show</button>
     </>
   )
 };
