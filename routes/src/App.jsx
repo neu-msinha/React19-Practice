aec94a36b7621e7e299315c4043d91eeac411bbf
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-17 22:45:52 +0530

    Better header using bootstrap

--- routes/src/App.jsx
+++ routes/src/App.jsx
@@ -9,15 +9,19 @@ function App() {
     return (
         <BrowserRouter>
 
-        {/* this will be present all the time */}
-        <header>
-          <Link to="/">Home</Link><br/>
-          <Link to="/posts">Posts</Link><br/>
-          <Link to={{
-            pathname: '/profile',
-            hash: '#mayukh',
-            search: '?profile=true'
-          }}>Profile</Link><br/>
+        <div className='container'>
+         {/* this will be present all the time */}
+        <header className='d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom'>
+
+          <Link to="/" className='d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none'>
+            <span className='fs-4'>My App</span>
+          </Link>
+
+          <ul className='nav nav-pills'>
+            <li className='nav-item'><Link to="/" className='nav-link'>Home</Link></li>
+            <li className='nav-item'><Link to="/posts" className='nav-link'>Posts</Link></li>
+            <li className='nav-item'><Link to="/profile" className='nav-link'>Profile</Link></li>
+          </ul>
         </header>
 
         {/* these are the routes */}
@@ -26,6 +30,8 @@ function App() {
           <Route path='posts' element={<Posts/>}/>
           <Route path='profile' element={<Profile/>}/>
         </Routes>
+
+        </div>
         </BrowserRouter>
   )
 }
