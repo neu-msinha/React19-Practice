43ce95abd12c9389af8287f1ab79462ee14204e8
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-19 02:11:56 +0530

    Added validation error message and used field component

--- forms/src/components/formThree.jsx
+++ forms/src/components/formThree.jsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const FormThree = () => {
@@ -6,6 +6,7 @@ const FormThree = () => {
     const formikProps = {
             initialValues: {
                 firstname: '',
+                color: ''
             },
             validationSchema: Yup.object({
                 firstname: Yup.string()
@@ -29,7 +30,23 @@ const FormThree = () => {
                         type="text" 
                         name="firstname"
                     />
-                    
+                    <ErrorMessage name="firstname" component="div" className="text-danger" />
+                    <hr className="mb-4" />
+
+                    <label htmlFor="color">Color</label>
+                    <Field 
+                        className="custom-select" 
+                        as="select" 
+                        name="color"
+                    >
+                        <option values="red">Red</option>
+                        <option values="green">Green</option>
+                        <option values="blue">Blue</option>
+                        <option values="yellow">Yellow</option>
+                    </Field>
+
+                    <ErrorMessage name="color" component="div" className="text-danger" />
+
                     <hr className="mb-4" />
                     <button className="btn btn-primary btn-lg btn-block" type="submit">
                         Submit
