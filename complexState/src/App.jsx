58d59f2a78f78a3018e39b7822ccc771e13f93ae
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-19 20:43:29 +0530

    Step 2: Utilizing data from slice in an App using useSelector Hook

--- complexState/src/App.jsx
+++ complexState/src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+import { useSelector } from "react-redux"
 
+const App = () => {
+
+  const movies = useSelector((state) => state.movies.list);
+
+  console.log(movies);
 
   return (
     <>
-      APP
+      <h1>Movie List</h1>
+      <ul>
+        {movies.length === 0 ? (
+          <li>No movies available</li>
+        ) : (
+          movies.map((movie, index) => (
+            <li key={index}>
+              {movie.title}
+              &nbsp;  
+            </li>
+          ))
+        )}
+      </ul>
     </>
   )
 }
