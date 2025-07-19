e7a04405580d7a8afa9b482470bce042b95ccdb2
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-19 20:18:43 +0530

    Step1.1: Creating reducer(using slice) and importing it in store

---
+++ complexState/src/store/movies.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const moviesSlice = createSlice({
+  name: 'movies',
+  initialState: {},
+  reducers: {
+    // Define your reducers here
+  }   
+});
+
+export default moviesSlice.reducer;
